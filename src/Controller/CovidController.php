0ec9b021c70ce1cebcbd65fd7ca626adff7f990a
Albert Barreto <albert.barreto@outlook.com>
2020-04-24 21:30:42 -0300

    HttpClient countries

--- src/Controller/CovidController.php
+++ src/Controller/CovidController.php
@@ -5,15 +5,36 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/covid", name="covid")
+ */
 class CovidController extends AbstractController
 {
     /**
-     * @Route("/covid", name="covid")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render('covid/index.html.twig', [
             'controller_name' => 'CovidController',
+        ]);
+    }
+
+    /**
+     * @Route("/countries", name="countries")
+     */
+    public function countries()
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://api.covid19api.com/countries');
+
+        $statusCode = $response->getStatusCode();
+        $contentType = $response->getHeaders()['content-type'][0];
+        $content = $response->getContent();
+        $content = $response->toArray();
+
+        return $this->json([
+            'data' => $content
         ]);
     }
 
